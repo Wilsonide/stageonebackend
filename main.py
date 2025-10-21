f182ebe9dea080a0c28409f35eab3d5fc92f76a0
Wilson Icheku <ichekuwilson538@gmail.com>
2025-10-21 23:48:17 +0100

    up and running

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ app = FastAPI(title=config.app_name)
 
 # Register routes
 app.include_router(service.router, prefix="/api")
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
